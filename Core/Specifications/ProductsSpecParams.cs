67d79f2a1952a0d79e2ac0d23f6543642cad3ed1
satyasahu2021 <satyendra.sahu2021@outlook.com>
2022-08-06 15:05:44 +0530

    End of section 9

--- Core/Specifications/ProductsSpecParams.cs
+++ Core/Specifications/ProductsSpecParams.cs
@@ -26,7 +26,7 @@ namespace Core.Specifications
 
         private string _search;
 
-        public string Search
+        public string? Search
         {
             get => _search;
             set => _search = value.ToLower();
