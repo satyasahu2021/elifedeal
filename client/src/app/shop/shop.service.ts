1a093a6ef4b0ec2b20fee9d2ff4ae9fe3a67b93d
satyasahu2021 <satyendra.sahu2021@outlook.com>
2022-08-07 10:40:58 +0530

    End of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import {map} from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ import { ShopParams } from '../shared/models/shopParams';
 export class ShopService {
  baseUrl ='https://localhost:5001/api/'
   constructor(private http: HttpClient) { }
+
 
   getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
@@ -25,7 +27,7 @@ export class ShopService {
 
     if(shopParams.search){
       params = params.append('search',shopParams.search);
-      
+
     }
 
       params =params.append('sort', shopParams.sort);
@@ -42,6 +44,12 @@ export class ShopService {
     );
 
   }
+
+  getProduct(id:number){
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
+
+
   getBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
   }
