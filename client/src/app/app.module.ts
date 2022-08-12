955d973f2bfaf2c38aa276d458bf51ed78b1f8b8
satyasahu2021 <satyendra.sahu2021@outlook.com>
2022-08-12 13:43:12 +0530

    End of section 12

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { CoreModule } from './core/core.module';
 //import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptors';
 
 
 @NgModule({
@@ -23,10 +25,12 @@ import { ErrorInterceptor } from './core/interceptors/error.interceptor';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    HomeModule
+    HomeModule,
+    NgxSpinnerModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { sha1 } from '@angular/compiler/src/i18n/digest';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -9,7 +11,8 @@ import { HomeComponent } from './home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports:[HomeComponent]
 })
